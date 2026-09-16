9a2908080184077775261cc1ce70c3fbe78b12e7
Helen Song <helenhsong@gmail.com>
2026-09-16 10:46:51 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/blueprint/',
+  build: {
+    rollupOptions: {
+      input: ['index.html', 'readme/index.html'],
+    },
+  },
+  plugins: [react()],
+})
